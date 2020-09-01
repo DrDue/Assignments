99e73c211cbd1090dba72b46d029c4dbac779af0
DrDue <dr.due98@gmail.com>
2020-09-01 11:38:16 +0200

    add coin

--- js/js15.js
+++
@@ -1,4 +0,0 @@
-var f = prompt("enter the value of fahrenheit", "fahrenheit");
-var c = 5 / 9 * (f - 32);
-z = c.toFixed(1);
-document.write(z + " degrees");
--- js/js201.js
+++
@@ -1,4 +0,0 @@
-var year = prompt("År")
-var leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
-console.log(leapYear)
-document.write("skudår: " + leapYear);
